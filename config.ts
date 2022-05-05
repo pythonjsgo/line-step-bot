491ed045c6c85c81db5477fa34c5385f69d3873d
Alexander <64742133+pythonjsgo@users.noreply.github.com>
2022-05-06 00:39:02 +0300

    Delete config.ts

--- config.ts
+++
@@ -1,8 +0,0 @@
-export const TOKEN = '5359728354:AAG-4cL2zEnHyfBkauWiXc8KW35XLcQORbg'
-export const DB_NAME = 'lite-step'
-export const MONGO_PASSWORD = 'D6g5YAmLZZuUQMX4'
-export const MONGO_URI = `mongodb+srv://admin:${MONGO_PASSWORD}@cluster0.jrqpx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-export const MINIMUM_WITHDRAWAL_AMOUNT = 2000
-export const WITHDRAWAL_CHAT_ID = -1001543637129
-export const BONUS_PER_ONE_REFERRAL = 100
-export const BOT_LINK = 't.me/Lite_Step_bot'
